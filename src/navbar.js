0deddd5ef6c8ae5b1569c5c20f04f1b2cb203c4b
Biniyam Badege <biniyam.b@ienetworks.co>
2021-05-24 03:56:02 -0400

    inital stage

--- src/navbar.js
+++ src/navbar.js
@@ -1,14 +1,11 @@
+import { Link } from 'react-router-dom';
 const Navbar = () => {
     return (  
         <nav className="navbar">
             <h1 className="links">The Dojo Blog</h1>
             <div className="content">
-                <a href="/">Home</a>
-                <a href="/create" style={{
-                    color: "white",
-                    backgroundColor: "#f1356d",
-                    borderRadius: '8px'
-                }}>New Blog</a>
+                <Link to="/">Home</Link>
+                <Link to="/create">New Blog</Link>
             </div>
         
         
